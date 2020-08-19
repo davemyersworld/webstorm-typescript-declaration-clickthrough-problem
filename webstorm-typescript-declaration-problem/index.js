e3c08941078e2f3d0970fe7957c55125adb6f40b
Dave Myers <dmyers@squarespace.com>
2020-08-19 11:57:57 -0400

    Change ts file to js to demonstrate specific problem

--- webstorm-typescript-declaration-problem/index.js
+++ webstorm-typescript-declaration-problem/index.js
@@ -1,6 +1,8 @@
-// This is my App, i'm dependent on a typescript module in a library
+// This is my App, i'm dependent on a javascript module in a library
+// that also offers typescript declarations
 // But I cannot click through to the actual implementation
 import TypescriptDependency from 'webstorm-typescript-declaration-dependency';
 
-// Here, a Javascript dependency, I can navigate to the underlying code
+// Here, a Javascript dependency w/o ts declarations,
+// I can navigate to the underlying code
 import JavascriptDependency from 'webstorm-javascript-dependency';
